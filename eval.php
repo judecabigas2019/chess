fa7069616eb0d964dbd47b2be7318c62b8a045d6
judecabigas2019 <47166011+judecabigas2019@users.noreply.github.com>
2025-05-15 11:48:33 +0800

    Create eval.php

---
+++ eval.php
@@ -0,0 +1,39 @@
+<?php
+$fen = $_GET['fen'] ?? null;
+
+if (!$fen) {
+    http_response_code(400);
+    echo "Missing FEN parameter.";
+    exit;
+}
+
+$cmd = './stockfish';
+$descriptorspec = [
+    0 => ["pipe", "r"], // stdin
+    1 => ["pipe", "w"], // stdout
+    2 => ["pipe", "w"]  // stderr
+];
+$process = proc_open($cmd, $descriptorspec, $pipes);
+
+if (is_resource($process)) {
+    fwrite($pipes[0], "position fen $fen\n");
+    fwrite($pipes[0], "go depth 15\n");
+    sleep(1); // wait for engine to respond
+    fwrite($pipes[0], "quit\n");
+    fclose($pipes[0]);
+
+    $output = stream_get_contents($pipes[1]);
+    fclose($pipes[1]);
+    fclose($pipes[2]);
+
+    proc_close($process);
+
+    // Get best move from output
+    if (preg_match("/bestmove\s+(\w+)/", $output, $matches)) {
+        echo json_encode(["best_move" => $matches[1]]);
+    } else {
+        echo json_encode(["error" => "Could not parse move."]);
+    }
+} else {
+    echo json_encode(["error" => "Failed to run Stockfish."]);
+}
